f09ecd19e063f03c46abeb000c1771b20c289f22
dkqpqp2 <dkqpqp2@gmail.com>
2026-03-05 14:55:46 +0900

    Commit

---
+++ Source/OneHandedSwordMaster/Character/Enemy/OHSMEnemyWhite.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OHSMEnemyBase.h"
+#include "OHSMEnemyWhite.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONEHANDEDSWORDMASTER_API AOHSMEnemyWhite : public AOHSMEnemyBase
+{
+	GENERATED_BODY()
+	
+};
